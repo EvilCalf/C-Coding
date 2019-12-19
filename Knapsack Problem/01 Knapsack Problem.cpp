49c91f458d09c68c6f061a1df8e233047721940a
EvilCalf <xujiading1997@163.com>
2019-12-19 10:17:12 +0800

    add Knapsack Problem

---
+++ Knapsack Problem/01 Knapsack Problem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+unsigned int f[10];
+unsigned int weight[10];
+unsigned int value[10];
+#define max(x, y) (x) > (y) ? (x) : (y)
+int main()
+{
+    int N, M;
+    cin >> N; //物品个数
+    cin >> M; //背包容量
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> weight[i] >> value[i];
+    }
+    for (int i = 1; i <= N; i++)
+        for (int j = M; j >= weight[i]; j--)
+            f[j] = max(f[j], f[j - weight[i]] + value[i]);
+    cout << f[M] << endl; //输出最优解
+}
